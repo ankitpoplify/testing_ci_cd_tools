4655cff6bba0b7be68393c327c08acc434f02010
Ankit Singh <ankitsingh@poplify.com>
2023-12-14 12:48:26 +0530

    ios app setup

---
+++ testing_ci_cd_tools/testing_ci_cd_tools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testing_ci_cd_tools
+//
+//  Created by Poplify on 14/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
